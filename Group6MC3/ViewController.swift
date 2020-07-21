b4fe9630539433070768ad10fb49bfea70ef54b3
Faris Ali Yafie <farisaliyafie@gmail.com>
2020-07-21 09:47:17 +0700

    Initial Commit

---
+++ Group6MC3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Group6MC3
+//
+//  Created by Faris Ali Yafie on 21/07/20.
+//  Copyright © 2020 Faris Ali Yafie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
