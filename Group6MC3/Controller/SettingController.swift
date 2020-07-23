aeee625f91c710608b04f5053e9721c916d7fe8d
Faris Ali Yafie <farisaliyafie@gmail.com>
2020-07-23 14:19:01 +0700

    add local notification for reminder part 2

--- Group6MC3/Controller/SettingController.swift
+++ Group6MC3/Controller/SettingController.swift
@@ -23,44 +23,60 @@ class SettingController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        reminder.content.sound = .default
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func switchAction(_ sender: UISwitch) {
+    
+    // Start : All Switch Actions
+    @IBAction func alcoholSwitched(_ sender: UISwitch) {
         if(switchAlcohol.isOn){
             print("no alcohol")
             reminder.avoidAlcoholReminder()
         }
+    }
+    @IBAction func eatSwitched(_ sender: UISwitch) {
         if(switchEat.isOn){
             print("no eat")
             reminder.avoidEatReminder()
         }
+    }
+    @IBAction func caffeineSwitched(_ sender: UISwitch) {
         if(switchCaffeine.isOn){
             print("no caffeine")
             reminder.avoidFaffeineReminder()
         }
+    }
+    @IBAction func exerciseSwitched(_ sender: UISwitch) {
         if(switchExercise.isOn){
             print("no exercise")
             reminder.avoidExerciseReminder()
         }
+    }
+    @IBAction func sugarSwitched(_ sender: UISwitch) {
         if(switchSugar.isOn){
             print("no sugar")
             reminder.avoidSugarReminder()
         }
+    }
+    @IBAction func gadgetSwitched(_ sender: UISwitch) {
         if(switchGadget.isOn){
             print("no gadget")
             reminder.avoidGadgetReminder()
         }
+    }
+    @IBAction func bedSwitched(_ sender: UISwitch) {
         if(switchBed.isOn){
             print("reminder for bed time")
             reminder.bedTimeReminder()
         }
+    }
+    @IBAction func encouragementSwitched(_ sender: UISwitch) {
         if(switchEncouragement.isOn){
             print("For better life")
             reminder.encouragementReminder()
         }
-        reminder.notification()
     }
+    // End: All Switch Actions
     
     @IBAction func soundSetting(_ sender: UITapGestureRecognizer) {
     }
--- ReminderController.swift
+++ ReminderController.swift
@@ -5,79 +5,98 @@
 //  Created by Faris Ali Yafie on 23/07/20.
 //  Copyright © 2020 Faris Ali Yafie. All rights reserved.
 //
-
-import UIKit
 import UserNotifications
 
 // This class has function to give user notifications
-class ReminderController: UIViewController {
+class ReminderController{
 
     // 1. Ask for permission
     let center = UNUserNotificationCenter.current()
     // 2. Create the notification content
     let content = UNMutableNotificationContent()
-    // 3. Create the notification trigger
-    let date = Date().addingTimeInterval(8)
-    // 4. Create the request
+    // 3. Create the notification trigger like...
+    let date = Date().addingTimeInterval(15)
+    // 4. Create the request like...
     let uuidStr = UUID().uuidString
     
-    func notification(){
-        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
-        }
-        
-        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+    func avoidAlcoholReminder(){
+        content.title = "Avoid Alcohol"
+        content.body = "No more alcohol until sleep time"
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date().addingTimeInterval(10))
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-        let request = UNNotificationRequest(identifier: uuidStr, content: content, trigger: trigger)
-        
-        // 5. Register the request
+        let uuidAlcohol = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidAlcohol, content: content, trigger: trigger)
         center.add(request) { (error) in
             //Check the parameter and handle any errors
         }
     }
-    
-    func avoidAlcoholReminder() -> UNMutableNotificationContent{
-        content.title = "Avoid Alcohol"
-        content.body = "No more alcohol until sleep time"
-        return content
-    }
-    func avoidEatReminder() -> UNMutableNotificationContent{
+    func avoidEatReminder(){
         content.title = "Avoid Late Eat"
         content.body = "No more eat until sleep time"
-        return content
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date().addingTimeInterval(15))
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let uuidEat = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidEat, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            //Check the parameter and handle any errors
+        }
     }
-    func avoidFaffeineReminder() -> UNMutableNotificationContent{
+    func avoidFaffeineReminder(){
         content.title = "Avoid Caffeine"
         content.body = "No more caffeine consume until sleep time"
-        return content
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date().addingTimeInterval(20))
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let uuidCaffeine = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidCaffeine, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            //Check the parameter and handle any errors
+        }
     }
-    func avoidExerciseReminder() -> UNMutableNotificationContent{
+    func avoidExerciseReminder(){
         content.title = "Avoid Exercise"
         content.body = "No more exercise until sleep time"
-        return content
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date().addingTimeInterval(25))
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let uuidExercise = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidExercise, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            //Check the parameter and handle any errors
+        }
     }
-    func avoidSugarReminder() -> UNMutableNotificationContent{
+    func avoidSugarReminder(){
         content.title = "Avoid Sugar"
         content.body = "No more sugar consume until sleep time"
-        return content
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date().addingTimeInterval(30))
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let uuidSugar = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidSugar, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            //Check the parameter and handle any errors
+        }
     }
-    func avoidGadgetReminder() -> UNMutableNotificationContent{
+    func avoidGadgetReminder(){
         content.title = "Avoid Gadget"
         content.body = "No more gadget use until sleep time"
-        return content
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date().addingTimeInterval(35))
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let uuidGadget = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidGadget, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            //Check the parameter and handle any errors
+        }
     }
-    func bedTimeReminder() -> UNMutableNotificationContent{
+    func bedTimeReminder(){
         content.title = "Bed Time"
         content.body = "Prepare for sleep time"
-        return content
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let uuidBed = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidBed, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            //Check the parameter and handle any errors
+        }
     }
     func encouragementReminder(){
         
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
 }
