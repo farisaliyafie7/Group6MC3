9d1feb93048b2a04e2c9dd6d7166a2862353ca2f
Faris Ali Yafie <farisaliyafie@gmail.com>
2020-07-24 10:56:20 +0700

    - add StatisticVC and connect to Clock.Storyboard - change title and subtitle in setting page from label to image view (pick assets from sketch)

---
+++ Group6MC3/Controller/StatisticVC.swift
@@ -0,0 +1,30 @@
+//
+//  StatisticVC.swift
+//  Group6MC3
+//
+//  Created by Faris Ali Yafie on 24/07/20.
+//  Copyright © 2020 Faris Ali Yafie. All rights reserved.
+//
+
+import UIKit
+
+class StatisticVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
